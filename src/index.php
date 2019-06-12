eec97f657792ac61b91ac92278932a332da5ce24
onnyprima <onnyprima@gmail.com>
2019-06-12 16:31:54 +0700

    remove old file

--- src/index.php
+++ src/index.php
@@ -2,57 +2,8 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use Security\Lib\Services\Model\Perangkat;
-use Security\Lib\Services\Model\Akun;
-use Security\Lib\Services\Mysql\MysqlOperation;
-use Security\Lib\Services\Mssql\MssqlOperation;
-
 class Test
 {
-
-    protected $operation;
-
-    public function __construct()
-    {
-        $operation = new MssqlOperation();
-        $this->operation = $operation;
-    }
-
-    public function addPerangkat()
-    {
-        $perangkat = new Perangkat();
-
-        $perangkat->setNomor("123456");
-        $perangkat->setDeskripsi("Test");
-
-        $this->operation->addPerangkat($perangkat);
-    }
-
-    public function addAkun()
-    {
-        $akun = new Akun();
-        $akun->setIdAkun("onny");
-        $akun->setDeskripsi("Test");
-        $this->operation->addAkun($akun);
-    }
-
-    public function addPerangkatDanAkun()
-    {
-        /**
-         * select by id dulu biar dapat id untuk relasi
-         */
-        $perangkat = new Perangkat();
-        $perangkat->setNomor(123456);
-        $akun = new Akun();
-        $akun->setIdAkun("onny");
-        $this->operation->addPerangkatDanAkun($perangkat, $akun);
-    }
-
-    public function testSqlSrv()
-    {
-        $conn = new \Security\Config\Connection();
-        $koneksi = $conn->pdoSqlSrvConn();
-    }
 
     /**
      * Contoh tambah perangkat
@@ -122,12 +73,13 @@ class Test
             'token' => md5(time()),
             'id_perangkat_dan_akun' => 5,
             'expired' => date("Y-m-d H:i:s"),
-            'login_at' =>  date("Y-m-d H:i:s"),
+            'login_at' => date("Y-m-d H:i:s"),
             'logout_at' => null
         ];
         $result = $createSession->create($data);
         return $result;
     }
+
 }
 
 $test = new Test();
